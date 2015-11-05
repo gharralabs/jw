5e1bb1b660ca3135888c72a480ddcc0a0bc304c6
Marine King <mkp@prime.co.kr>
2015-11-05 17:17:05 -0200

    Canvas - Lista de Amigos

---
+++ Facebook API/Canvas/Canvas/Controllers/JogoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Canvas.Controllers
+{
+    public class JogoController : Controller
+    {
+        // GET: Jogo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Facebook API/Canvas/Canvas/Scripts/Jogo/Index.js
@@ -0,0 +1,40 @@
+﻿
+  window.fbAsyncInit = function() {
+      FB.init({
+          appId      : '1479920475649433',
+          xfbml      : true,
+          version    : 'v2.5'
+      });
+
+      
+      FB.getLoginStatus(function (response) {
+          // logado no FB e na aplicação
+          if (response.status === 'connected') {
+              alert('conectado');
+          }
+              // logado no FB mas não na aplicação
+          else if (response.status === 'not_authorized') {
+              alert('não auto');
+              FB.login(function (response) {
+                  console.log(response);
+              }, { scope: 'public_profile,email,friend_list' });
+
+          }
+              // Não está logado no facebook
+          else {
+              alert('não está conectado');
+          }
+      });
+
+
+
+
+  };
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
